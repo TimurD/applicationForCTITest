cdb6e09decdd30bd8799854844291771a966623c
Oleksandr_Minchuk <oleksandr_minchuk@epam.com>
2018-02-21 14:58:12 +0200

    fix token: remove invalid characters. add logging for requests

--- src/main/java/com/mock_cti/Application.java
+++ src/main/java/com/mock_cti/Application.java
@@ -2,9 +2,24 @@ package com.mock_cti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class Application {
+
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setIncludeHeaders(true);
+		filter.setIncludeClientInfo(true);
+
+		filter.setMaxPayloadLength(10000);
+		return filter;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/com/mock_cti/controllers/MainController.java
+++ src/main/java/com/mock_cti/controllers/MainController.java
@@ -12,42 +12,27 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/")
 public class MainController {
-	private static final String OAUTH_RESPONSE_BODY = "<ResponseMessage>\n" +
-			"<AccessToken>\n" +
-			"Bearer eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ0ZW5hbnQiLCJpYXQiOjE1MTc1NTU0MzAsImV4cCI6MTUxNzU1NTQzNH0.GmFApUzZbcug2xMpeMMxRMf268IT5zsTIvKPnzEumGEJGRwPtnKF6Rikfw1mYYEoqObBBZrdNJv3_-L8_XbjD_9urSI51Cir6g3xO4hef9FKMMH0boLZF7zDjcKmLKSdUlC1mfDBmRdSZ0XfHFP7OocvCFM2kprFVQDQlBoHYH2GLk055utyfHQox6ibkipokz9TptKN4LiziOnfuMNnarxFPUS-vmgQBZf64spzzVuuOtqEGPM6Rptb0WrQ-pplUMVKMNyoPbjVa-auCUAOhXRF3kEX3mmQ95PrZSSeKGB1Yvn-_gebNyBoi9-Gp62xFPovtctPwC5FOB1ZGgfSmw\n" +
-			"</AccessToken>\n" +
-			"<ExpiresInMinutes>\n" +
-			"3600\n" +
-			"</ExpiresInMinutes >\n" +
-			"</ResponseMessage>\n";
+	private static final String OAUTH_RESPONSE_BODY = "<ResponseMessage>" +
+				"<AccessToken>" +
+					"Bearer eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ0ZW5hbnQiLCJpYXQiOjE1MTc1NTU0MzAsImV4cCI6MTUxNzU1NTQzNH0.GmFApUzZbcug2xMpeMMxRMf268IT5zsTIvKPnzEumGEJGRwPtnKF6Rikfw1mYYEoqObBBZrdNJv3_-L8_XbjD_9urSI51Cir6g3xO4hef9FKMMH0boLZF7zDjcKmLKSdUlC1mfDBmRdSZ0XfHFP7OocvCFM2kprFVQDQlBoHYH2GLk055utyfHQox6ibkipokz9TptKN4LiziOnfuMNnarxFPUS-vmgQBZf64spzzVuuOtqEGPM6Rptb0WrQ-pplUMVKMNyoPbjVa-auCUAOhXRF3kEX3mmQ95PrZSSeKGB1Yvn-_gebNyBoi9-Gp62xFPovtctPwC5FOB1ZGgfSmw" +
+				"</AccessToken>" +
+				"<ExpiresInMinutes>" +
+					"3600" +
+				"</ExpiresInMinutes >" +
+			"</ResponseMessage>";
 	private final Logger LOGGER = LogManager.getLogger(MainController.class);
 
 	@Autowired
 	XmlParsingService xmlParsingService;
 
 	@PostMapping
-	public ResponseEntity printxmlToLog(@RequestHeader HttpHeaders headers,
-										@RequestBody String xmlBody) throws Exception {
-
-		StringBuilder builder = new StringBuilder("Request headers:\n");
-		headers.keySet().forEach(it ->
-				builder.append("\t" + it.toString() + ":" + headers.get(it).toString() + "\n")
-		);
-		LOGGER.info(builder.toString());
-
-		LOGGER.info("Request body:\n" + xmlBody);
-
+	public ResponseEntity printXmlToLog(@RequestHeader HttpHeaders headers, @RequestBody String xmlBody) {
 		return xmlParsingService.parseXml(xmlBody);
 	}
 
 	@GetMapping
-	public String returnOAuth2Token(@RequestHeader("Tenant-Ref") String tenantRef,
-									@RequestHeader("Username") String username,
-									@RequestHeader("Password") String password
-	) {
+	public String returnOAuth2Token(@RequestHeader HttpHeaders headers) {
 		LOGGER.info("OAuth2 token request received");
-
-
 		return OAUTH_RESPONSE_BODY;
 	}
 }
